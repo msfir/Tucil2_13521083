12b9634305e38eb721ac5a7a7a051bdd74b1dd43
msfir <ms.firdaus777@gmail.com>
2023-02-28 00:01:35 +0700

    Add proper input/output

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tucil/stima/pairit
 
 go 1.19
+
+require (
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,26 +6,49 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"time"
+	"tucil/stima/pairit/algorithm"
 	"tucil/stima/pairit/point"
+
+	. "github.com/klauspost/cpuid/v2"
 )
 
-const SIZE = 100
+var upperBound float32
 
-func generatePoints(n int) []point.Point3D {
+func generatePoints(n int, ub float32) []point.Point3D {
+	upperBound = ub
 	points := make([]point.Point3D, n)
 	for i := 0; i < n; i++ {
-		x := (rand.Float32() - 0.5) * SIZE
-		y := (rand.Float32() - 0.5) * SIZE
-		z := (rand.Float32() - 0.5) * SIZE
+		x := rand.Float32() * upperBound
+		y := rand.Float32() * upperBound
+		z := rand.Float32() * upperBound
 		points[i] = point.CreatePoint3D(x, y, z)
 	}
 	return points
 }
 
 func main() {
-	n := 100
+	var n int
 
-	points := generatePoints(n)
+input:
+	fmt.Print("Number of points: ")
+	_, err := fmt.Scan(&n)
+	if err != nil || n < 2 {
+		fmt.Println("Invalid input!")
+		goto input // Is it bad practice? I don't think so
+	}
+
+	points := generatePoints(n, float32(n))
+
+	start := time.Now()
+	p1, p2, d := algorithm.FindClosestPoint3DPair(points)
+	executionTime := time.Since(start)
+	fmt.Println("====== Closest Pair ======")
+	fmt.Printf("Point 1: (%v, %v %v)\n", p1.GetX(), p1.GetY(), p1.GetZ())
+	fmt.Printf("Point 2: (%v, %v %v)\n", p2.GetX(), p2.GetY(), p2.GetZ())
+	fmt.Printf("Distance: %f\n", d)
+	fmt.Printf("Execution time: %f s (%s)\n", float64(executionTime.Nanoseconds())/1e9, CPU.BrandName)
+	fmt.Printf("The Euclidean distance function is called %dx\n", point.NumOfCalls)
 
 	path, err := exec.LookPath("gnuplot")
 	if err != nil {
@@ -42,6 +65,9 @@ func main() {
 			y := points[i].GetY()
 			z := points[i].GetZ()
 			color := 0
+			if &points[i] == p1 || &points[i] == p2 {
+				color = 7
+			}
 			line := fmt.Sprintf("%v %v %v %v\n", x, y, z, color)
 			if _, err := data.WriteString(line); err != nil {
 				err_str := fmt.Sprintf("** %v\n", err)
@@ -55,10 +81,11 @@ func main() {
 			err_str := fmt.Sprintf("** %v\n", err)
 			panic(err_str)
 		}
-		cmd := fmt.Sprintf("splot \"%s\" u 1:2:3:4 t \"points\" w p pt 7 ps 1 lc variable", data.Name())
-		plotCmd(stdin, fmt.Sprintf("set xrange [-%[1]d:%[1]d]", SIZE/2))
-		plotCmd(stdin, fmt.Sprintf("set yrange [-%[1]d:%[1]d]", SIZE/2))
-		plotCmd(stdin, fmt.Sprintf("set zrange [-%[1]d:%[1]d]", SIZE/2))
+		cmd := fmt.Sprintf("splot \"%s\" u 1:2:3:4 t \"\" w p pt 7 ps 1 lc variable", data.Name())
+		plotCmd(stdin, "set term qt title 'PairIt'")
+		plotCmd(stdin, fmt.Sprintf("set xrange [0:%f]", upperBound))
+		plotCmd(stdin, fmt.Sprintf("set yrange [0:%f]", upperBound))
+		plotCmd(stdin, fmt.Sprintf("set zrange [0:%f]", upperBound))
 		plotCmd(stdin, cmd)
 		plotCmd(stdin, "pause mouse close")
 		plotCmd(stdin, "q")
