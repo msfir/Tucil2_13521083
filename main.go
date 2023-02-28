394d1b92e1a02b73b8c0e312d719db3a589cc5f2
msfir <ms.firdaus777@gmail.com>
2023-02-28 16:02:07 +0700

    Print banner

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ func generatePoints(n int, ub float64) []point.Point {
 }
 
 func main() {
+	printBanner()
+
 	var n int
 
 input:
@@ -43,21 +45,34 @@ input:
 	start := time.Now()
 	p1, p2, d := algorithm.FindClosestPointPair(points)
 	executionTime := time.Since(start)
-	fmt.Println("====== Closest Pair ======")
-	fmt.Printf("Point 1: (%v, %v, %v)\n", p1.GetCoord()[0], p1.GetCoord()[1], p1.GetCoord()[2])
-	fmt.Printf("Point 2: (%v, %v, %v)\n", p2.GetCoord()[0], p2.GetCoord()[1], p2.GetCoord()[2])
-	fmt.Printf("Distance: %f\n", d)
-	fmt.Printf("Execution time: %f s (%s)\n", float64(executionTime.Nanoseconds())/1e9, CPU.BrandName)
+	fmt.Println("\x1b[93m====== Divide and Conquer ======\x1b[0m")
+	fmt.Printf("Point 1\t\t: (%v, %v, %v)\n", p1.GetCoord()[0], p1.GetCoord()[1], p1.GetCoord()[2])
+	fmt.Printf("Point 2\t\t: (%v, %v, %v)\n", p2.GetCoord()[0], p2.GetCoord()[1], p2.GetCoord()[2])
+	fmt.Printf("Distance\t: %f\n", d)
+	fmt.Printf("Execution time\t: %.9f s (%s)\n", float64(executionTime.Nanoseconds())/1e9, CPU.BrandName)
+	fmt.Printf("The Euclidean distance function is called %dx\n", point.NumOfCalls)
+
+	point.NumOfCalls = 0
+	fmt.Println()
+
+	start = time.Now()
+	p1, p2, d = algorithm.BruteForceFCP(points)
+	executionTime = time.Since(start)
+	fmt.Println("\x1b[93m====== Brute Force ======\x1b[0m")
+	fmt.Printf("Point 1\t\t: (%v, %v, %v)\n", p1.GetCoord()[0], p1.GetCoord()[1], p1.GetCoord()[2])
+	fmt.Printf("Point 2\t\t: (%v, %v, %v)\n", p2.GetCoord()[0], p2.GetCoord()[1], p2.GetCoord()[2])
+	fmt.Printf("Distance\t: %f\n", d)
+	fmt.Printf("Execution time\t: %.9f s (%s)\n", float64(executionTime.Nanoseconds())/1e9, CPU.BrandName)
 	fmt.Printf("The Euclidean distance function is called %dx\n", point.NumOfCalls)
 
 	path, err := exec.LookPath("gnuplot")
 	if err != nil {
-		fmt.Printf("** %v\n", err)
+		fmt.Printf("** %v\n", err.Error())
 	} else {
 		data, err := os.CreateTemp(".", "gnuplot-data-")
 		defer os.Remove(data.Name())
 		if err != nil {
-			err_str := fmt.Sprintf("** %v\n", err)
+			err_str := fmt.Sprintf("** %v\n", err.Error())
 			panic(err_str)
 		}
 		for i := 0; i < n; i++ {
@@ -70,7 +85,7 @@ input:
 			}
 			line := fmt.Sprintf("%v %v %v %v\n", x, y, z, color)
 			if _, err := data.WriteString(line); err != nil {
-				err_str := fmt.Sprintf("** %v\n", err)
+				err_str := fmt.Sprintf("** %v\n", err.Error())
 				panic(err_str)
 			}
 		}
@@ -78,7 +93,7 @@ input:
 		proc := exec.Command(path)
 		stdin, _ := proc.StdinPipe()
 		if proc.Start() != nil {
-			err_str := fmt.Sprintf("** %v\n", err)
+			err_str := fmt.Sprintf("** %v\n", err.Error())
 			panic(err_str)
 		}
 		cmd := fmt.Sprintf("splot \"%s\" u 1:2:3:4 t \"\" w p pt 7 ps 1 lc variable", data.Name())
@@ -90,7 +105,7 @@ input:
 		plotCmd(stdin, "pause mouse close")
 		plotCmd(stdin, "q")
 		if proc.Wait() != nil {
-			err_str := fmt.Sprintf("** %v\n", err)
+			err_str := fmt.Sprintf("** %s\n", err.Error())
 			panic(err_str)
 		}
 	}
@@ -98,6 +113,14 @@ input:
 
 func plotCmd(stdin io.Writer, command string) {
 	if _, err := io.WriteString(stdin, command+"\n"); err != nil {
-		fmt.Printf("** %v\n", err)
+		fmt.Printf("** %s\n", err.Error())
+	}
+}
+
+func printBanner() {
+	if banner, err := os.ReadFile("banner"); err != nil {
+		fmt.Println("** ", err.Error())
+	} else {
+		fmt.Println(string(banner))
 	}
 }
